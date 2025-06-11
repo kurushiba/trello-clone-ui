4c2bc8a28471fbd4f165de413669bf9dc007cd3e
kurushiba <156669290+kurushiba@users.noreply.github.com>
2025-06-11 18:18:29 +0900

    Update SortableList.tsx

--- Home/SortableBoard/SortableList.tsx
+++ Home/SortableBoard/SortableList.tsx
@@ -5,7 +5,12 @@ export function SortableList() {
   return (
     <div>
       <div className={`list`}>
-        <div className="list-header">
+          <div
+            className="list-header"
+            style={{
+              cursor: 'grab',
+            }}
+          >
           <h3 className="list-title">テストリスト</h3>
           <button className="list-options">
             <svg viewBox="0 0 24 24" width="16" height="16" fill="currentColor">
